08fac24431e94baa58048798af0ebc8f8ce6c3bc
lenurb-123 <kpogbemebrunel@gmail.com>
2025-05-06 17:19:15 +0100

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,12 +1,25 @@
-def menu_principal():
-    print("==== DorkScope404 ====")
-    print("1. Rechercher un Dork")
-    print("2. Quitter")
-    choix = input("Choisissez une option : ")
+# cli.py
+from dork_generator import generer_tous_les_dorks
 
-    if choix == "1":
-        print("Fonction de dork à venir...")
-    elif choix == "2":
-        print("Au revoir !")
-    else:
-        print("Option invalide.")
+def menu_principal():
+    while True:
+        print("\n=== DorkScope CLI ===")
+        print("1. Générer des Dorks")
+        print("2. Quitter")
+        
+        choix = input("Choisissez une option : ")
+
+        if choix == "1":
+            domaine = input("Entrez le domaine (ex : facebook.com) : ")
+            dorks = generer_tous_les_dorks(domaine)
+            print("\nDorks générés :\n")
+            for dork in dorks:
+                print(dork)
+        elif choix == "2":
+            print("Au revoir!")
+            break
+        else:
+            print("[!] Option invalide. Essayez à nouveau.")
+
+if __name__ == "__main__":
+    menu_principal()
